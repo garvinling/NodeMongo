f981eb3b2d05136957cbdd82a9b83c202541cab1
Garvin <garvin.ling@student.csulb.edu>
2014-01-23 01:45:35 -0800

    Linked MongoDB to read users

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
+
+//Adding MongoDB line (Using NodeLogin_1 as DB)
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/NodeLogin_1');
+
+
+
+
+
 var app = express();
 
 // all environments
@@ -21,16 +31,43 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views/')));
+
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+//app.get('/', routes.index);
+
 app.get('/users', user.list);
+app.get('/userlist',routes.userlist(db));
+
+
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,20 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.userlist = function(db){
+
+	return function(req,res){
+
+		var collection = db.get('usercollection');
+		collection.find({},{},function(e,docs){
+
+			res.render('userlist',{
+
+				"userlist" : docs
+
+			});
+
+		});
+	};
+};
